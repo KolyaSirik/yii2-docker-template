a914be7ec92b16098e2baa778fb35fd891160856
andreyons <andrew.sokirko@bigdropinc.com>
2017-05-12 13:55:15 +0300

    done: yii2 template with docker integrated

--- install-once.sh
+++ install-once.sh
@@ -8,3 +8,9 @@ if [ -z ${PROJECT_NAME} ] || [ -z ${PORT} ] ; then
     echo 'Invalid arguments!'
     exit 1
 fi
+
+find config -type f -exec sed -i "s/example_project/$PROJECT_NAME/g" {} +
+find app/environments -type f -exec sed -i "s/example_project/$PROJECT_NAME/g" {} +
+find config/common-compose.tml -exec sed -i "s/12421/$PORT/g" {} +
+echo 'Done!'
+echo 'Have a nice day!'
