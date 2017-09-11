7fbfb848d3687095518c5bf5782e5ae6c4232193
andreyons <andrew.sokirko@bigdropinc.com>
2017-09-11 11:58:57 +0300

    fix: tests

--- app/frontend/models/PasswordResetRequestForm.php
+++ app/frontend/models/PasswordResetRequestForm.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace frontend\models;
 
 use Yii;
@@ -23,9 +24,9 @@ class PasswordResetRequestForm extends Model
             ['email', 'required'],
             ['email', 'email'],
             ['email', 'exist',
-                'targetClass' => '\common\models\User',
-                'filter' => ['status' => User::STATUS_ACTIVE],
-                'message' => 'There is no user with this email address.'
+             'targetClass' => '\common\models\User',
+             'filter'      => ['status' => User::STATUS['ACTIVE']],
+             'message'     => 'There is no user with this email address.',
             ],
         ];
     }
@@ -39,14 +40,14 @@ class PasswordResetRequestForm extends Model
     {
         /* @var $user User */
         $user = User::findOne([
-            'status' => User::STATUS_ACTIVE,
-            'email' => $this->email,
+            'status' => User::STATUS['ACTIVE'],
+            'email'  => $this->email,
         ]);
 
         if (!$user) {
             return false;
         }
-        
+
         if (!User::isPasswordResetTokenValid($user->password_reset_token)) {
             $user->generatePasswordResetToken();
             if (!$user->save()) {
