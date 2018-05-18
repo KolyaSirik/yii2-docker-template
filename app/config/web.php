2310646c22856039b2263d5fa1af01b165072dda
Kolya Siryk <nikolay.siryk@bigdropinc.com>
2018-05-18 16:00:25 +0300

    update env

--- app/config/web.php
+++ app/config/web.php
@@ -1,6 +1,6 @@
 <?php
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+defined('YII_DEBUG') or define('YII_DEBUG', env('YII_DEBUG') === 'true');
+defined('YII_ENV') or define('YII_ENV', env('YII_ENV'));
 
 $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
